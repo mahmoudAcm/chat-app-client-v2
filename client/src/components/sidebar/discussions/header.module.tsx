c2adf6eef755bf69261f98f1f7dfd642940c53d0
mahmoudAcm <mahmoud.acm.123@gamil.com>
2020-10-29 20:09:04 +0200

    removning discussions header for now

--- client/src/components/sidebar/discussions/header.module.tsx
+++ client/src/components/sidebar/discussions/header.module.tsx
@@ -1,36 +1,21 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const DiscussionsHeader = () => {
   return (
     <header>
-      <div className="search">
-        <form className="form-inline position-relative">
-          <input
-            type="search"
-            className="form-control"
-            id="conversations"
-            placeholder="Search for conversations..."
-          />
-          <button type="button" className="btn btn-link loop">
-            <i className="material-icons">search</i>
-          </button>
-        </form>
-        <button
-          className="btn create"
-          data-toggle="modal"
-          data-target="#startnewchat"
-        >
-          <i className="material-icons">create</i>
-        </button>
-      </div>
       <div className="list-group sort">
-        <button
-          className="btn filterDiscussionsBtn active show"
-          data-toggle="list"
-          data-filter="all"
+        <NavLink
+          to="/discussions"
+          className="btn filterDiscussionsBtn"
+          isActive={(match, { search }) => {
+            const is = '' === search;
+            return is;
+          }}
+          activeClassName="active show"
         >
           All
-        </button>
+        </NavLink>
       </div>
     </header>
   );
